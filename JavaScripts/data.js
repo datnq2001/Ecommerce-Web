efcde72f9d92773959a632c3d4b064b33f0c59e7
datnq2001 <datnq2001@gmail.com>
2024-05-10 20:15:35 +1000

    Update Effect

--- JavaScripts/data.js
+++ JavaScripts/data.js
@@ -1,116 +1,116 @@
 var sanPham = [
   {
-      "name": "Valini Coffee", "price": 50, "img": "./img/ImgThucUong/VaniliCoffee.jpg", "id": "clickMua1", "remove": "clickXoa1", "count": 1
+      "name": "Valini Coffee", "price": 9, "img": "./img/ImgThucUong/VaniliCoffee.jpg", "id": "clickMua1", "remove": "clickXoa1", "count": 1
   },
   {
-      "name": "Cocktail Tea", "price": 55, "img": "./img/ImgThucUong/TraCocktail.jpg", "id": "clickMua2", "remove": "clickXoa2", "count": 1
+      "name": "Cocktail Tea", "price": 11, "img": "./img/ImgThucUong/TraCocktail.jpg", "id": "clickMua2", "remove": "clickXoa2", "count": 1
   },
   {
-      "name": "Strawberry Crushed", "price": 45, "img": "./img/ImgThucUong/DauEp.jpg", "id": "clickMua3", "remove": "clickXoa3", "count": 1
+      "name": "Strawberry Crushed", "price": 8, "img": "./img/ImgThucUong/DauEp.jpg", "id": "clickMua3", "remove": "clickXoa3", "count": 1
   },
   {
-      "name": "Chocolate Coffee", "price": 40, "img": "./img/ImgThucUong/ChocolateCoffee.jpg", "id": "clickMua4", "remove": "clickXoa4", "count": 1
+      "name": "Chocolate Coffee", "price": 7.5, "img": "./img/ImgThucUong/ChocolateCoffee.jpg", "id": "clickMua4", "remove": "clickXoa4", "count": 1
   },
   {
-      "name": "Cappucchino", "price": 37, "img": "./img/ImgThucUong/Cappucchino.png", "id": "clickMua5", "remove": "clickXoa5", "count": 1
+      "name": "Cappucchino", "price": 7, "img": "./img/ImgThucUong/Cappucchino.png", "id": "clickMua5", "remove": "clickXoa5", "count": 1
   },
   {
-      "name": "Matcha Iceblended", "price": 35, "img": "./img/ImgThucUong/TraXanhDaXay.png", "id": "clickMua6", "remove": "clickXoa6", "count": 1
+      "name": "Matcha Iceblended", "price": 9, "img": "./img/ImgThucUong/TraXanhDaXay.png", "id": "clickMua6", "remove": "clickXoa6", "count": 1
   },
   {
-      "name": "Iced Chocolate", "price": 45, "img": "./img/ImgThucUong/SocolaKemDaXay.png", "id": "clickMua7", "remove": "clickXoa7", "count": 1
+      "name": "Iced Chocolate", "price": 8, "img": "./img/ImgThucUong/SocolaKemDaXay.png", "id": "clickMua7", "remove": "clickXoa7", "count": 1
   },
   {
-      "name": "Peach Tea", "price": 40, "img": "./img/ImgThucUong/TraDao.png", "id": "clickMua8", "remove": "clickXoa8", "count": 1
+      "name": "Peach Tea", "price": 7.5, "img": "./img/ImgThucUong/TraDao.png", "id": "clickMua8", "remove": "clickXoa8", "count": 1
   },
   {
-      "name": "Chocolate Iceblended", "price": 50, "img": "./img/ImgThucUong/ChocolataDaSay.png", "id": "clickMua9", "remove": "clickXoa9", "count": 1
+      "name": "Chocolate Iceblended", "price": 9, "img": "./img/ImgThucUong/ChocolataDaSay.png", "id": "clickMua9", "remove": "clickXoa9", "count": 1
   },
   {
-      "name": "Milk Tea", "price": 48, "img": "./img/ImgThucUong/TraSua.jpg", "id": "clickMua10", "remove": "clickXoa10", "count": 1
+      "name": "Milk Tea", "price": 8.75, "img": "./img/ImgThucUong/TraSua.jpg", "id": "clickMua10", "remove": "clickXoa10", "count": 1
   },
   {
-      "name": "Lotus Logan Tea", "price": 45, "img": "./img/ImgThucUong/TraVaiSen.jpg", "id": "clickMua11", "remove": "clickXoa11", "count": 1
+      "name": "Lotus Logan Tea", "price": 8, "img": "./img/ImgThucUong/TraVaiSen.jpg", "id": "clickMua11", "remove": "clickXoa11", "count": 1
   },
   {
-      "name": "Ice Blend Coffee", "price": 45, "img": "./img/ImgThucUong/ICEBLENDED.png", "id": "clickMua12", "remove": "clickXoa12", "count": 1
+      "name": "Ice Blend Coffee", "price": 8, "img": "./img/ImgThucUong/ICEBLENDED.png", "id": "clickMua12", "remove": "clickXoa12", "count": 1
   },
   {
-      "name": "Caramel Frappuchino", "price": 53, "img": "./img/ImgThucUong/CoffeeCaramelDaXay.png", "id": "clickMua13", "remove": "clickXoa13", "count": 1
+      "name": "Caramel Frappuchino", "price": 7, "img": "./img/ImgThucUong/CoffeeCaramelDaXay.png", "id": "clickMua13", "remove": "clickXoa13", "count": 1
   },
   {
-      "name": "Caramel Iceblended", "price": 49, "img": "./img/ImgThucUong/CaramelDaXay.png", "id": "clickMua14", "remove": "clickXoa14", "count": 1
+      "name": "Caramel Iceblended", "price": 9, "img": "./img/ImgThucUong/CaramelDaXay.png", "id": "clickMua14", "remove": "clickXoa14", "count": 1
   },
   {
-      "name": "Apple and Strawberry Juice", "price": 56, "img": "./img/ImgThucUong/TaoVaDauEp.jpg", "id": "clickMua15", "remove": "clickXoa15", "count": 1
+      "name": "Apple and Strawberry Juice", "price": 7.5, "img": "./img/ImgThucUong/TaoVaDauEp.jpg", "id": "clickMua15", "remove": "clickXoa15", "count": 1
   },
   {
-      "name": "Grapefruit Juice", "price": 55, "img": "./img/ImgThucUong/BuoiEp.png", "id": "clickMua16", "remove": "clickXoa16", "count": 1
+      "name": "Grapefruit Juice", "price": 7.5, "img": "./img/ImgThucUong/BuoiEp.png", "id": "clickMua16", "remove": "clickXoa16", "count": 1
   },
   {
-      "name": "Orange Juice", "price": 40, "img": "./img/ImgThucUong/CamEp.png", "id": "clickMua17", "remove": "clickXoa17", "count": 1
+      "name": "Orange Juice", "price": 7.5, "img": "./img/ImgThucUong/CamEp.png", "id": "clickMua17", "remove": "clickXoa17", "count": 1
   },
   {
-      "name": "Guava Juice", "price": 45, "img": "./img/ImgThucUong/OiEp.png", "id": "clickMua18", "remove": "clickXoa18", "count": 1
+      "name": "Guava Juice", "price": 8, "img": "./img/ImgThucUong/OiEp.png", "id": "clickMua18", "remove": "clickXoa18", "count": 1
   },
   {
-      "name": "Mango Smoothie", "price": 46, "img": "./img/ImgThucUong/SinhToXoai.png", "id": "clickMua19", "remove": "clickXoa19", "count": 1
+      "name": "Mango Smoothie", "price": 9, "img": "./img/ImgThucUong/SinhToXoai.png", "id": "clickMua19", "remove": "clickXoa19", "count": 1
   },
   {
-      "name": "Lemon Smoothie", "price": 42, "img": "./img/ImgThucUong/SinhToChanh.png", "id": "clickMua20", "remove": "clickXoa20", "count": 1
+      "name": "Lemon Smoothie", "price": 7.75, "img": "./img/ImgThucUong/SinhToChanh.png", "id": "clickMua20", "remove": "clickXoa20", "count": 1
   },
   {
-      "name": "Banana Smoothie", "price": 48, "img": "./img/ImgThucUong/SinhToChuoi.png", "id": "clickMua21", "remove": "clickXoa21", "count": 1
+      "name": "Banana Smoothie", "price": 8.75, "img": "./img/ImgThucUong/SinhToChuoi.png", "id": "clickMua21", "remove": "clickXoa21", "count": 1
   },
   {
-      "name": "Oreo and Coffee Blend", "price": 51, "img": "./img/ImgThucUong/BanhOreo.png", "id": "clickMua22", "remove": "clickXoa22", "count": 1
+      "name": "Oreo and Coffee Blend", "price": 9, "img": "./img/ImgThucUong/BanhOreo.png", "id": "clickMua22", "remove": "clickXoa22", "count": 1
   },
   {
-      "name": "Milk Coffee", "price": 35, "img": "./img/ImgThucUong/CoffeeSua.png", "id": "clickMua23", "remove": "clickXoa23", "count": 1
+      "name": "Milk Coffee", "price": 9, "img": "./img/ImgThucUong/CoffeeSua.png", "id": "clickMua23", "remove": "clickXoa23", "count": 1
   },
   {
-      "name": "Cappuccino", "price": 35, "img": "./img/ImgThucUong/Cappuccino.png", "id": "clickMua24", "remove": "clickXoa24", "count": 1
+      "name": "Cappuccino", "price": 9, "img": "./img/ImgThucUong/Cappuccino.png", "id": "clickMua24", "remove": "clickXoa24", "count": 1
   },
   {
-      "name": "Chocolate", "price": 40, "img": "./img/ImgThucUong/Socola.png", "id": "clickMua25", "remove": "clickXoa25", "count": 1
+      "name": "Chocolate", "price": 7.5, "img": "./img/ImgThucUong/Socola.png", "id": "clickMua25", "remove": "clickXoa25", "count": 1
   },
   {
-      "name": "Latte", "price": 45, "img": "./img/ImgThucUong/Latte.png", "id": "clickMua26", "remove": "clickXoa26", "count": 1
+      "name": "Latte", "price": 8, "img": "./img/ImgThucUong/Latte.png", "id": "clickMua26", "remove": "clickXoa26", "count": 1
   },
   {
-      "name": "Caramel latte", "price": 45, "img": "./img/ImgThucUong/Caramel.png", "id": "clickMua27", "remove": "clickXoa27", "count": 1
+      "name": "Caramel latte", "price": 8, "img": "./img/ImgThucUong/Caramel.png", "id": "clickMua27", "remove": "clickXoa27", "count": 1
   },
   {
-      "name": "Vanilla Coffee", "price": 42, "img": "./img/ImgThucUong/Caramel.png", "id": "clickMua28", "remove": "clickXoa28", "count": 1
+      "name": "Vanilla Coffee", "price": 7, "img": "./img/ImgThucUong/Caramel.png", "id": "clickMua28", "remove": "clickXoa28", "count": 1
   },
   {
-      "name": "Jasmine Tea", "price": 30, "img": "./img/ImgThucUong/TraXanh.png", "id": "clickMua29", "remove": "clickXoa29", "count": 1
+      "name": "Jasmine Tea", "price": 7, "img": "./img/ImgThucUong/TraXanh.png", "id": "clickMua29", "remove": "clickXoa29", "count": 1
   },
   {
-      "name": "Green Tea", "price": 32, "img": "./img/ImgThucUong/TraXanh.png", "id": "clickMua30", "remove": "clickXoa30", "count": 1
+      "name": "Green Tea", "price": 7, "img": "./img/ImgThucUong/TraXanh.png", "id": "clickMua30", "remove": "clickXoa30", "count": 1
   },
   {
-      "name": "Olong Tea", "price": 35000, "img": "./img/ImgThucUong/TraXanh.png", "id": "clickMua31", "remove": "clickXoa31", "count": 1
+      "name": "Olong Tea", "price": 7, "img": "./img/ImgThucUong/TraXanh.png", "id": "clickMua31", "remove": "clickXoa31", "count": 1
   },
   {
-      "name": "Lotus Tea", "price": 37000, "img": "./img/ImgThucUong/TraXanh.png", "id": "clickMua32", "remove": "clickXoa32", "count": 1
+      "name": "Lotus Tea", "price": 7, "img": "./img/ImgThucUong/TraXanh.png", "id": "clickMua32", "remove": "clickXoa32", "count": 1
   },
   {
-      "name": "Black Tea With Milk Foam", "price": 55000, "img": "./img/ImgThucUong/TraDen.png", "id": "clickMua33", "remove": "clickXoa33", "count": 1
+      "name": "Black Tea With Milk Foam", "price": 8.5, "img": "./img/ImgThucUong/TraDen.png", "id": "clickMua33", "remove": "clickXoa33", "count": 1
   },
   {
-      "name": "Pineapple Strawberry Mix", "price": 50000, "img": "./img/ImgThucUong/ThomDauEp.png", "id": "clickMua34", "remove": "clickXoa34", "count": 1
+      "name": "Pineapple Strawberry Mix", "price": 8.5, "img": "./img/ImgThucUong/ThomDauEp.png", "id": "clickMua34", "remove": "clickXoa34", "count": 1
   },
   {
-      "name": "Apple Juice", "price": 45000, "img": "./img/ImgThucUong/TaoEp.png", "id": "clickMua35", "remove": "clickXoa35", "count": 1
+      "name": "Apple Juice", "price": 8, "img": "./img/ImgThucUong/TaoEp.png", "id": "clickMua35", "remove": "clickXoa35", "count": 1
   },
   {
-      "name": "Pineapple juice", "price": 46000, "img": "./img/ImgThucUong/DuaEp.png", "id": "clickMua36", "remove": "clickXoa36", "count": 1
+      "name": "Pineapple juice", "price": 8, "img": "./img/ImgThucUong/DuaEp.png", "id": "clickMua36", "remove": "clickXoa36", "count": 1
   },
   {
-      "name": "Vitamin C (Mango, Orange)", "price": 55000, "img": "./img/ImgThucUong/VitaminC.png", "id": "clickMua37", "remove": "clickXoa37", "count": 1
+      "name": "Vitamin C (Mango, Orange)", "price": 8.5, "img": "./img/ImgThucUong/VitaminC.png", "id": "clickMua37", "remove": "clickXoa37", "count": 1
   },
   {
-      "name": "Tropical Fruit Smoothie", "price": 56000, "img": "./img/ImgThucUong/NhietDoi.png", "id": "clickMua38", "remove": "clickXoa38", "count": 1
+      "name": "Tropical Fruit Smoothie", "price": 8.5, "img": "./img/ImgThucUong/NhietDoi.png", "id": "clickMua38", "remove": "clickXoa38", "count": 1
   }
 ];
